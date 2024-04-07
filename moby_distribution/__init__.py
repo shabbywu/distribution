bee4814c06404ea52eb2a44031a099afea94e0ef
shabbywu <shabbywu@tencent.com>
2024-04-07 16:04:59 +0800

    support pydantic 2.x

--- moby_distribution/__init__.py
+++ moby_distribution/__init__.py
@@ -1,19 +1,28 @@
-from moby_distribution.registry.client import DockerRegistryV2Client, default_client, set_default_client
+from moby_distribution.registry.client import (
+    DockerRegistryV2Client,
+    default_client,
+    set_default_client,
+)
 from moby_distribution.registry.resources.blobs import Blob
 from moby_distribution.registry.resources.image import ImageRef, LayerRef
 from moby_distribution.registry.resources.manifests import ManifestRef
 from moby_distribution.registry.resources.tags import Tags
 from moby_distribution.spec.endpoint import OFFICIAL_ENDPOINT, APIEndpoint
 from moby_distribution.spec.image_json import ImageJSON
-from moby_distribution.spec.manifest import ManifestSchema1, ManifestSchema2, OCIManifestSchema1
+from moby_distribution.spec.manifest import (
+    ManifestSchema1,
+    ManifestSchema2,
+    OCIManifestSchema1,
+)
 
-__version__ = "0.7.0"
-__ALL__ = [
+__version__ = "0.7.1"
+__all__ = [
     "DockerRegistryV2Client",
     "Blob",
     "ManifestRef",
     "Tags",
     "APIEndpoint",
+    "OFFICIAL_ENDPOINT",
     "ManifestSchema1",
     "ManifestSchema2",
     "OCIManifestSchema1",
